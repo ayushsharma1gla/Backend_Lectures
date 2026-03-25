efbee9174e111de866892834dd47850c43e78173
Ayush Sharma <ayushas4177@gmail.com>
2026-03-25 12:19:02 +0530

    Add server2.js

--- index4.js
+++ index4.js
@@ -19,6 +19,7 @@ app.get('/api/users/:id', (req, res) => {
   return res.json(user);
 });
 
+
 app.post('/api/users', (req, res) => {
   const newUser = {
     id: users.length + 1,
@@ -27,6 +28,29 @@ app.post('/api/users', (req, res) => {
   users.push(newUser);
   return res.json(users);
 });
+
+app.patch("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => user.id == id);
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  Object.assign(user, req.body);
+  return res.json(users);
+});
+
+
+
+app.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  const index = users.findIndex((u) => u.id === id);  
+  if (index === -1) {
+    return res.status(404).json({ message: 'user not found' });
+  }
+  users.splice(index, 1);
+  return res.json({ message: 'user deleted' });
+});
+
 
 app.listen(3000, () => {
   console.log('Server listening on http://localhost:3000');
---
+++ index5.js
@@ -0,0 +1,7 @@
+const student = ["Divyanshu","Praveen", "Sanjay"];
+const deletedStudent  = student.splice(1,1);
+
+console.log(students);
+console.log(deletedStudent);
+
+    
